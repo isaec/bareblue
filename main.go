0ae07c3ea2a09461207c40ccf0c78718b5ce5e28
isaac <isaac@rubenfamily.com>
2020-11-28 21:11:16 -0800

    small bugfixes and improvments
    yeah ik this isnt descriptive
    want a savepoint before i try to make the mocked text tied to the names

--- main.go
+++ main.go
@@ -20,7 +20,27 @@ func main() {
 
 	chats := mockChats()
 
-	chatHeader := widget.NewLabelWithStyle("select a chat", fyne.TextAlignCenter, fyne.TextStyle{true, false, true})
+	badDb := make(map[string][]fyne.CanvasObject)
+
+	for i := range chats {
+		badDb[chats[i]] = mockChatMessages(25)
+	}
+
+	chatHeader := widget.NewLabelWithStyle("select a chat", fyne.TextAlignCenter, fyne.TextStyle{Bold: true, Italic: false, Monospace: true})
+
+	//message entry
+
+	messageEntry := widget.NewEntry()
+
+	messageEntry.SetPlaceHolder("bareblue")
+
+	chatMessages := []fyne.CanvasObject{}
+
+	chatContent := container.NewVScroll(widget.NewVBox(chatMessages...))
+
+	content := fyne.NewContainerWithLayout(layout.NewBorderLayout(chatHeader, messageEntry, nil, nil), chatHeader, messageEntry, chatContent)
+
+	//end message entry
 
 	//messageList block
 	messageList := widget.NewList(
@@ -32,18 +52,19 @@ func main() {
 			contactIcon.FillMode = canvas.ImageFillContain
 			contactIcon.SetMinSize(fyne.NewSize(40, 40))
 
-			lastMessage := widget.NewLabelWithStyle("last message lalalalalaalalalal", fyne.TextAlignCenter, fyne.TextStyle{false, true, false})
+			lastMessage := widget.NewLabelWithStyle("last message", fyne.TextAlignCenter, fyne.TextStyle{Bold: false, Italic: true, Monospace: false})
 			lastMessage.Wrapping = fyne.TextTruncate
 
 			return fyne.NewContainerWithLayout(layout.NewHBoxLayout(),
 				contactIcon,
 				fyne.NewContainerWithLayout(layout.NewVBoxLayout(),
-					widget.NewLabelWithStyle("Template Chat", fyne.TextAlignCenter, fyne.TextStyle{true, false, false}),
+					widget.NewLabelWithStyle("Template Chat", fyne.TextAlignCenter, fyne.TextStyle{Bold: true, Italic: false, Monospace: false}),
 					lastMessage))
 		},
 
 		func(id widget.ListItemID, item fyne.CanvasObject) {
 			item.(*fyne.Container).Objects[1].(*fyne.Container).Objects[0].(*widget.Label).SetText(chats[id])
+			chatMessages = badDb[chats[id]]
 		})
 
 	messageList.OnSelected = func(id widget.ListItemID) {
@@ -51,22 +72,11 @@ func main() {
 	}
 	//end messageList block
 
-	messageEntry := widget.NewEntry()
-
-	messageEntry.SetPlaceHolder("bareblue")
-
-	chatMessages := mockChatMessages(25)
-
-	chatContent := container.NewVScroll(widget.NewVBox(chatMessages...))
-
-	chatContent.ScrollToBottom()
-
-	content := fyne.NewContainerWithLayout(layout.NewBorderLayout(chatHeader, messageEntry, nil, nil), chatHeader, messageEntry, chatContent)
-
 	messageAndContent := fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, nil, messageList, nil), messageList, content)
 
 	w.SetContent(messageAndContent)
 	w.Resize(fyne.NewSize(1200, 600))
 	addMenus(&a, &w)
 	w.ShowAndRun()
+	//chatContent.ScrollToBottom()
 }
